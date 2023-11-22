7a5e73c71ab6d3497131272e7b8528f2214a0998
JaroslawX <ad.jaro@wp.pl>
2023-11-22 20:25:10 +0100

    First inits.

---
+++ Models/AddressBook.py
@@ -0,0 +1,6 @@
+class AddressBook:
+    def __init__(self):
+        self.contacts = []
+
+    def add_contact(self, contact):
+        self.contacts.append(contact)
---
+++ Models/Contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, first_name, last_name, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+
+    def __str__(self):
+        return f"First Name: {self.first_name}, Last Name: {self.last_name}, Phone Number: {self.phone_number}, Email: {self.email}"
