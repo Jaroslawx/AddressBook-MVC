bee4084a4146a857eede75ce5603ea582935a97d
JaroslawX <ad.jaro@wp.pl>
2023-11-22 20:44:18 +0100

    Logs update, and check in main.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from utils.Log import AppLogger, logger
+
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
+    logger.log_info("Application started.")
     print_hi('PyCharm')
 
---
+++ utils/Log.py
@@ -0,0 +1,36 @@
+import logging
+import os
+
+
+class AppLogger:
+    def __init__(self, log_file):
+        self.logger = logging.getLogger(__name__)
+
+        # Prevent adding handlers multiple times
+        if not self.logger.handlers:
+            self.logger.setLevel(logging.DEBUG)
+
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+            # Handler for file
+            log_folder = "files"
+            os.makedirs(log_folder, exist_ok=True)
+            log_path = os.path.join(log_folder, log_file)
+            file_handler = logging.FileHandler(log_path)
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
+
+    def log_info(self, message):
+        self.logger.info(message)
+
+    def log_warning(self, message):
+        self.logger.warning(message)
+
+    def log_error(self, message):
+        self.logger.error(message)
+
+    def log_critical(self, message):
+        self.logger.critical(message)
+
+
+logger = AppLogger('app.log')
