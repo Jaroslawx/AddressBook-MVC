86d20f60b6b735eabdb0db1f98b427e4c91bfa67
JaroslawX <ad.jaro@wp.pl>
2023-11-22 21:12:59 +0100

    updates, create classes.

---
+++ Models/Trivia.py
@@ -0,0 +1,26 @@
+from utils.Log import logger
+import random
+import os
+
+
+class Trivia:
+    def __init__(self, trivia_file="files/trivia.txt"):
+        self.trivia_file = trivia_file
+        self.trivia = self.load_trivia()
+
+    def load_trivia(self):
+        if os.path.exists(self.trivia_file):
+            with open(self.trivia_file, "r", encoding="utf-8") as file:
+                trivia_list = [line.strip() for line in file.readlines()]
+
+            logger.log_info(f"Loaded {len(trivia_list)} trivia.")
+            return trivia_list
+        else:
+            logger.log_error(f"File not found: {self.trivia_file}")
+            return []
+
+    def get_random_trivia(self):
+        return random.choice(self.trivia)
+
+
+trivia = Trivia()
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from utils.Log import AppLogger, logger
+from utils.Log import logger
+from Models.Trivia import trivia
 
 
-def print_hi(name):
-    print(f'Hi, {name}')
+def main():
+    logger.log_info("Application started.")
+    print(trivia.get_random_trivia())
+    logger.log_info("Application ended.")
 
 
 if __name__ == '__main__':
-    logger.log_info("Application started.")
-    print_hi('PyCharm')
-
+    main()
