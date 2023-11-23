c7645e812814e9a4135f89aa90c4da6f46bab073
JaroslawX <ad.jaro@wp.pl>
2023-11-23 15:58:33 +0100

    FileController add.

---
+++ Controllers/FileController.py
@@ -0,0 +1,52 @@
+from utils.Log import logger
+from Models.AddressBook import address_book
+from tkinter import filedialog
+from Models.Contact import Contact
+
+import tkinter as tk
+import csv
+
+
+class FileController:
+    @staticmethod
+    def load_contacts_and_add():
+        root = tk.Tk()
+        root.withdraw()  # Hide the main window
+
+        # Choose the file
+        file_path = filedialog.askopenfilename(
+            title="Select a file with contacts",
+            filetypes=[("Text files", "*.txt"), ("CSV files", "*.csv"), ("All files", "*.*")]
+        )
+
+        if file_path:
+            try:
+                contacts = FileController.load_contacts_from_file(file_path)
+                for contact in contacts:
+                    address_book.add_contact(contact)
+                logger.log_info("Contacts loaded successfully!")
+            except FileNotFoundError as e:
+                logger.log_error(f"File '{file_path}' not found: {e}")
+                raise
+            finally:
+                root.destroy()
+
+    @staticmethod
+    def load_contacts_from_file(filename):
+        with open(filename, 'r') as file:
+            reader = csv.reader(file, delimiter=';')
+            contacts = []
+            for row in reader:
+                contacts.append(Contact(row[0], row[1], row[2], row[3]))
+            return contacts
+
+    @staticmethod
+    def save_contacts_to_file(filename, contacts):
+        try:
+            with open(filename, 'w') as file:
+                for contact in contacts:
+                    file.write(f"{contact.first_name};{contact.last_name};{contact.phone_number};{contact.email}\n")
+            logger.log_info("Contacts saved successfully.")
+        except Exception as e:
+            logger.log_error(f"Error saving contacts to file: {e}")
+            raise
