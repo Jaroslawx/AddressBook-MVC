1e3526ce348009c69b238277bbd62ccbcd823650
JaroslawX <ad.jaro@wp.pl>
2023-12-02 14:18:16 +0100

    Added choose interface function

--- Controllers/Controller.py
+++ Controllers/Controller.py
@@ -1,3 +1,15 @@
 class Controller:
-    def __init__(self):
-        pass
+    @staticmethod
+    def choose_interface_mode():
+        print("Select interface mode:")
+        print("1. Textual Mode")
+        print("2. Graphic Mode")
+
+        while True:
+            choice = input("Enter your choice (1 or 2): ")
+            if choice == "1":
+                return "textual"
+            elif choice == "2":
+                return "graphic"
+            else:
+                print("Invalid choice. Please enter 1 or 2.")
