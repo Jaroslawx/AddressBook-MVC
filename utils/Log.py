219b87e469035ecf5463c95d7509330288c111b3
JaroslawX <ad.jaro@wp.pl>
2023-12-12 13:11:23 +0100

    add option to turn off logs

--- utils/Log.py
+++ utils/Log.py
@@ -3,8 +3,9 @@ import os
 
 
 class AppLogger:
-    def __init__(self, log_file):
+    def __init__(self, log_file, log_to_file=True):
         self.logger = logging.getLogger(__name__)
+        self.log_to_file = log_to_file
 
         # Prevent adding handlers multiple times
         if not self.logger.handlers:
@@ -12,13 +13,14 @@ class AppLogger:
 
             formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-            # Handler for file
-            log_folder = "files"
-            os.makedirs(log_folder, exist_ok=True)
-            log_path = os.path.join(log_folder, log_file)
-            file_handler = logging.FileHandler(log_path)
-            file_handler.setFormatter(formatter)
-            self.logger.addHandler(file_handler)
+            if self.log_to_file:
+                # Handler for file
+                log_folder = "files"
+                os.makedirs(log_folder, exist_ok=True)
+                log_path = os.path.join(log_folder, log_file)
+                file_handler = logging.FileHandler(log_path)
+                file_handler.setFormatter(formatter)
+                self.logger.addHandler(file_handler)
 
     def log_info(self, message):
         self.logger.info(message)
@@ -33,4 +35,4 @@ class AppLogger:
         self.logger.critical(message)
 
 
-logger = AppLogger('app.log')
+logger = AppLogger("app.log", log_to_file=False)
