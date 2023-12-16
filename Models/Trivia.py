df01a948f056a73964e969863e502f197f8dec87
JaroslawX <ad.jaro@wp.pl>
2023-12-16 09:02:20 +0100

    fixed bug when trivia file could not be found

--- Models/Trivia.py
+++ Models/Trivia.py
@@ -9,18 +9,22 @@ class Trivia:
         self.list_of_trivia = self.load_trivia()
 
     def load_trivia(self):
-        if os.path.exists(self.trivia_file):
-            with open(self.trivia_file, "r", encoding="utf-8") as file:
-                trivia_list = [line.strip() for line in file.readlines()]
+        try:
+            if os.path.exists(self.trivia_file):
+                with open(self.trivia_file, "r", encoding="utf-8") as file:
+                    trivia_list = [line.strip() for line in file.readlines()]
 
-            logger.log_info(f"Loaded {len(trivia_list)} trivia.")
-            return trivia_list
-        else:
-            logger.log_error(f"File not found: {self.trivia_file}")
-            return []
+                logger.log_info(f"Loaded {len(trivia_list)} trivia.")
+                return trivia_list
+            else:
+                raise FileNotFoundError(f"File not found: {self.trivia_file}")
+        except Exception as e:
+            logger.log_error(f"Error loading trivia: {e}")
+            return ["Error! Can't find any trivia."]
 
     def get_random_trivia(self):
         return random.choice(self.list_of_trivia)
 
 
 trivia = Trivia()
+
